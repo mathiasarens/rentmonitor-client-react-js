11ccb7670ebe8a912316628bd44493734c946695
Mathias Arens <mathias.arens@googlemail.com>
2019-07-17 22:07:58 +0100

    Intermedidate Tenant component checkin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Welcome from './welcome/Welcome'
 import Signup from './signup/Signup'
+import Tenant from './tenant/Tenant'
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -18,6 +19,7 @@ function App() {
       <Redirect exact from="/" to="/welcome" />
       <Route path="/welcome" component={Welcome} />
       <Route path="/signup" component={Signup} />
+      <Route path="/tenant" component={Tenant} />
       </Switch>
     </Router>
   );
---
+++ src/tenant/Tenant.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+function Signup() {
+  const classes = useStyles();
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          New Tenant
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoComplete="Name"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="email"
+            label="Email"
+            type="email"
+            id="email"
+            autoComplete="your@email.com"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="phone"
+            label="Phone"
+            type="phone"
+            id="phone"
+            autoComplete="+49 170 123456789"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Create
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
+
+export default Signup;
