c5c956dbd5f13004ec3585c60955648d06d31d35
Mathias Arens <mathias@arens-festers.de>
2021-01-18 22:59:25 +0100

    Readded .env file

--- deploy-pi0w.sh
+++ deploy-pi0w.sh
@@ -3,4 +3,4 @@
  docker build -t arm32v6/rentmonitor-client-reactjs:0.9.0 .
  docker save -o /tmp/rentmonitor-client-reactjs-0.9.0.img arm32v6/rentmonitor-client-reactjs:0.9.0
  scp /tmp/rentmonitor-client-reactjs-0.9.0.img pirate@pi0w:~/
- ssh pirate@pi0w "docker load -i rentmonitor-client-reactjs-0.9.0.img && docker run -it --rm -v ${PWD}:/app -v /app/node_modules -p 80:3000 -d arm32v6/rentmonitor-client-reactjs:0.9.0"
+ ssh pirate@pi0w "docker load -i rentmonitor-client-reactjs-0.9.0.img && docker run --rm -p 80:80 -d arm32v6/rentmonitor-client-reactjs:0.9.0"
