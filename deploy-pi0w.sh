464e1f2e49c61bc52a718919074c63891f3cfa02
Mathias Arens <mathias@arens-festers.de>
2021-03-15 23:16:04 +0100

    Updated version number to 0.9.1

--- deploy-pi0w.sh
+++ deploy-pi0w.sh
@@ -1,6 +1,6 @@
  #!/bin/zsh
 
- docker build -t arm32v6/rentmonitor-client-reactjs:0.9.0 .
- docker save -o /tmp/rentmonitor-client-reactjs-0.9.0.img arm32v6/rentmonitor-client-reactjs:0.9.0
- scp /tmp/rentmonitor-client-reactjs-0.9.0.img pirate@pi0w:~/
- ssh pirate@pi0w "docker load -i rentmonitor-client-reactjs-0.9.0.img && docker run --rm -p 80:80 -d arm32v6/rentmonitor-client-reactjs:0.9.0"
+ docker build -t arm32v6/rentmonitor-client-reactjs:0.9.1 .
+ docker save -o /tmp/rentmonitor-client-reactjs-0.9.1.img arm32v6/rentmonitor-client-reactjs:0.9.1
+ scp /tmp/rentmonitor-client-reactjs-0.9.1.img pirate@pi0w:~/
+ ssh pirate@pi0w "docker load -i rentmonitor-client-reactjs-0.9.1.img && docker run --rm -p 80:80 -d arm32v6/rentmonitor-client-reactjs:0.9.1"
