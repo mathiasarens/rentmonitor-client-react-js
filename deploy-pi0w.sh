dfebbbf917ef0217386bd71d0f956f719035ab2b
Mathias Arens <mathias@arens-festers.de>
2021-03-19 22:38:40 +0100

    Updated deploy shell file

--- deploy-pi0w.sh
+++ deploy-pi0w.sh
@@ -1,6 +1,10 @@
- #!/bin/zsh
-
- docker build -t arm32v6/rentmonitor-client-reactjs:0.9.1 .
- docker save -o /tmp/rentmonitor-client-reactjs-0.9.1.img arm32v6/rentmonitor-client-reactjs:0.9.1
- scp /tmp/rentmonitor-client-reactjs-0.9.1.img pirate@pi0w:~/
- ssh pirate@pi0w "docker load -i rentmonitor-client-reactjs-0.9.1.img && docker run --rm -p 80:80 -d arm32v6/rentmonitor-client-reactjs:0.9.1"
+#!/bin/zsh
+SECONDS=0
+VERSION=$(npx -c 'echo "$npm_package_version"')
+echo $VERSION
+docker build -t arm32v6/rentmonitor-client-reactjs:$VERSION .
+docker save -o /tmp/rentmonitor-client-reactjs-$VERSION.img arm32v6/rentmonitor-client-reactjs:$VERSION
+scp /tmp/rentmonitor-client-reactjs-$VERSION.img $DEPLOY_HOST:~/
+ssh $DEPLOY_HOST "echo 'Stopping rentmonitor-client...' && docker stop rentmonitor-client && echo 'Loading rentmonitor-client...' && docker load -i rentmonitor-client-reactjs-$VERSION.img && echo 'Running rentmonitor-client...' && docker run --rm -p 80:80 --name rentmonitor-client -d arm32v6/rentmonitor-client-reactjs:$VERSION && && echo Successfully deployed rentmonitor-client:$VERSION"
+DURATION=$SECONDS
+echo "rentmonitor-client:$VERSION deployment took $(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds."
