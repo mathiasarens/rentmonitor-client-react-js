2d52daef2f5d4704581c35860386c3769ddedc0a
Mathias Arens <mathias@arens-festers.de>
2021-04-04 06:19:21 +0200

    Introduced sm layout for tenant oveview

--- src/tenant/Tenant.js
+++ src/tenant/Tenant.js
@@ -4,7 +4,6 @@ import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import {makeStyles} from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
 import RefershIcon from '@material-ui/icons/Refresh';
@@ -20,17 +19,8 @@ import {DeleteConfirmationComponent} from '../utils/DeleteConfirmationComponent'
 import {openSnackbar} from '../utils/Notifier';
 import {tenantsLoader} from './dataaccess/tenantLoader';
 
-const useStyles = makeStyles((theme) => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-}));
-
 export default function Tenant() {
   const {t} = useTranslation();
-  const classes = useStyles();
   const [tenants, setTenants] = useState([]);
   const history = useHistory();
 
@@ -105,35 +95,43 @@ export default function Tenant() {
           </Grid>
         </Grid>
         {tenants.map((tenantListItem) => (
-          <div>
-            <Grid container marginTop={2} paddingTop={2}>
-              <Grid item xs={12}>
+          <div key={`tenant-${tenantListItem.id}`}>
+            <Grid
+              container
+              marginTop={2}
+              paddingTop={2}
+              spacing={1}
+              alignItems="flex-start"
+            >
+              <Grid item xs={12} sm={3}>
                 <Typography variant="h6">{tenantListItem.name}</Typography>
               </Grid>
-            </Grid>
-            <Grid container marginTop={1} spacing={1} alignItems="flex-end">
-              <Grid item xs={3}>
-                <Typography variant="caption">{t('email')}:</Typography>
+              <Grid item xs={12} sm={7}>
+                <Grid container spacing={0} marginTop={1} alignItems="flex-end">
+                  <Grid item xs={3}>
+                    <Typography variant="caption">{t('email')}:</Typography>
+                  </Grid>
+                  <Grid item xs={9}>
+                    {tenantListItem.email}
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography variant="caption">{t('phone')}:</Typography>
+                  </Grid>
+                  <Grid item xs={9}>
+                    {tenantListItem.phone}
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography variant="caption">
+                      {t('tenantAccountSynchronizationName')}:
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={9}>
+                    {tenantListItem.accountSynchronisationName}
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid item xs={9}>
-                {tenantListItem.email}
-              </Grid>
-              <Grid item xs={3}>
-                <Typography variant="caption">{t('phone')}:</Typography>
-              </Grid>
-              <Grid item xs={9}>
-                {tenantListItem.phone}
-              </Grid>
-              <Grid item xs={3}>
-                <Typography variant="caption">
-                  {t('tenantAccountSynchronizationName')}:
-                </Typography>
-              </Grid>
-              <Grid item xs={9}>
-                {tenantListItem.accountSynchronisationName}
-              </Grid>
-              <Grid item xs={12}>
-                <Grid container spacing={1}>
+              <Grid item xs={12} sm={2}>
+                <Grid container spacing={1} marginTop={1}>
                   <Grid item>
                     <Button
                       size="small"
