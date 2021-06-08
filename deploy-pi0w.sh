a1a62a0b97961996411ffcb0dd97b236954dd087
Mathias Arens <mathias.arens@googlemail.com>
2021-06-08 22:46:27 +0200

    Deploy script should support reboot safe docker runs

--- deploy-pi0w.sh
+++ deploy-pi0w.sh
@@ -5,6 +5,6 @@ echo $VERSION
 docker build -t arm32v6/rentmonitor-client-reactjs:$VERSION .
 docker save -o /tmp/rentmonitor-client-reactjs-$VERSION.img arm32v6/rentmonitor-client-reactjs:$VERSION
 scp /tmp/rentmonitor-client-reactjs-$VERSION.img $DEPLOY_HOST:~/
-ssh $DEPLOY_HOST "echo 'Stopping rentmonitor-client...' && docker stop rentmonitor-client && echo 'Loading rentmonitor-client...' && docker load -i rentmonitor-client-reactjs-$VERSION.img && echo 'Running rentmonitor-client...' && docker run --rm -p 80:80 --name rentmonitor-client -d arm32v6/rentmonitor-client-reactjs:$VERSION && echo 'Pruning unused images...' && docker image prune -af && echo Successfully deployed rentmonitor-client:$VERSION"
+ssh $DEPLOY_HOST "echo 'Stopping rentmonitor-client...' && docker stop rentmonitor-client && echo 'Removing rentmonitor-client...' && docker rm rentmonitor-client && echo 'Loading rentmonitor-client...' && docker load -i rentmonitor-client-reactjs-$VERSION.img && echo 'Running rentmonitor-client...' && docker run -p 80:80 --name rentmonitor-client -d --restart unless-stopped arm32v6/rentmonitor-client-reactjs:$VERSION && echo 'Pruning unused images...' && docker image prune -af && echo Successfully deployed rentmonitor-client:$VERSION"
 DURATION=$SECONDS
 echo "rentmonitor-client:$VERSION deployment took $(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds."
